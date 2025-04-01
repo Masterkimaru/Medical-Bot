5f71507d22d676ff4415999495e90c1460688eae
Masterkimaru <themasterskimaru@gmail.com>
2025-04-01 17:29:34 +0300

    some additions to app.py

--- app.py
+++ app.py
@@ -134,4 +134,4 @@ def clean_response(response):
     return response.strip()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
